efb3bcc69c768ba9738ac1ccda8f57ea5ef8c661
jcholewi <01161407@pw.edu.pl>
2026-01-12 20:36:08 +0100

    init new branch

--- src/sequentialgrid.py
+++ src/sequentialgrid.py
@@ -3,28 +3,56 @@ from matplotlib import pyplot as plt
 from grid import Grid
 from numba import njit, prange
 
+# class SequentialGrid(Grid):
+#     def __init__(self, n: int):
+#         super().__init__(n)
+
+#     def update(self, omega=1.7):
+#         max_diff = 0.0
+
+#         for i in range(1, self.n-1):
+#             for j in range(1, self.n-1):
+
+#                 old = self.T[i, j]
+#                 avg = 0.25 * ( self.T[i+1, j] + self.T[i-1, j] +
+#                             self.T[i, j+1] + self.T[i, j-1] )
+
+#                 new = (1 - omega) * old + omega * avg
+#                 self.T[i, j] = new
+
+#                 diff = abs(new - old)
+#                 if diff > max_diff:
+#                     max_diff = diff
+
+#         return max_diff
+    
+
 class SequentialGrid(Grid):
     def __init__(self, n: int):
         super().__init__(n)
 
-    def update(self, omega=1.7):
+    @staticmethod
+    @njit()
+    def _update(T, omega=0.96):
         max_diff = 0.0
+        n = T.shape[0]
+        for i in range(1, n-1):
+            for j in range(1, n-1):
 
-        for i in range(1, self.n-1):
-            for j in range(1, self.n-1):
-
-                old = self.T[i, j]
-                avg = 0.25 * ( self.T[i+1, j] + self.T[i-1, j] +
-                            self.T[i, j+1] + self.T[i, j-1] )
+                old = T[i, j]
+                avg = 0.25 * ( T[i+1, j] + T[i-1, j] +
+                            T[i, j+1] + T[i, j-1] )
 
                 new = (1 - omega) * old + omega * avg
-                self.T[i, j] = new
+                T[i, j] = new
 
-                diff = abs(new - old)
-                if diff > max_diff:
-                    max_diff = diff
-
-        return max_diff
+    def update(self, omega=0.96):
+        T_old = self.T.copy()
+        SequentialGrid._update(self.T, omega)
+        inner_old = T_old[1:-1, 1:-1]
+        inner_new = self.T[1:-1, 1:-1]
+        diff = np.max(np.abs(inner_new - inner_old))
+        return diff
 
 
 class AnalyticGrid(Grid):
